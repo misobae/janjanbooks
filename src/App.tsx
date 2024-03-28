f1500f997bd8039ed178b18ff732d43c780a5219
misobae <misobae0731@gmail.com>
2024-03-28 21:43:44 +0900

    Test: vercel 배포 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import axios from "axios";
+
 function App() {
-  return (
-    <div className="p-40">
-      <h1 className="text-3xl font-bold text-center">
-        Hello
-      </h1>
-    </div>
-  );
+    const fetchdata = async () => {
+      const { data } = await axios.get('/ttb/api/ItemSearch.aspx', {
+        params: {
+            ttbkey: process.env.ALADIN_API_KEY,
+            Query: 'aladin',
+            output: 'js'
+        }
+      });
+      console.log(data);
+    }
+    fetchdata();
+
+  return null;
 };
 
 export default App;
