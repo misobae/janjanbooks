865b57d47708e9b70c51f7146f0a70473d6a3b48
misobae <misobae0731@gmail.com>
2024-04-09 21:22:57 +0900

    Feat: 404 페이지 구현

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import Btn from "../components/common/Btn";
+
 function NotFound() {
   return (
-    <h1>NotFound</h1>
+    <div className="flex flex-col justify-center items-center  w-full h-screen">
+      <h1 className="text-8xl font-black tracking-tight">404</h1>
+      <p>Not Found</p>
+
+      <p className="text-center mt-3 mb-12">찾을 수 없는 페이지입니다. <br />입력하신 주소가 정확한지 다시 한 번 확인해 주세요.</p>
+      <div className="flex justify-center items-center gap-4">
+        <Link to="/">
+          <Btn text="홈으로 이동하기" />
+        </Link>
+      </div>
+    </div>
   )
 };
 
