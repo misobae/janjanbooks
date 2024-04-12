015adb4f8018e1ee8da90d8149218aac543e8d4f
misobae <misobae0731@gmail.com>
2024-04-12 13:42:13 +0900

    Feat: View 페이지 alert -> toast 수정

--- src/pages/RecordView.tsx
+++ src/pages/RecordView.tsx
@@ -10,6 +10,7 @@ import ProgressLabel from "../components/record/ProgressLabel";
 import ReadingPeriod from "../components/record/ReadingPeriod";
 import Modal from "../components/common/Modal";
 import BtnBack from "../components/common/BtnBack";
+import { notify } from "../components/common/Toast";
 
 import IconEdit from "../assets/images/icon_edit_gr.svg"
 import IconDel from "../assets/images/icon_delete.svg"
@@ -19,14 +20,24 @@ function RecordView() {
   
   const { id } = useParams(); 
   const [bookReviews, setBookReviews] = useRecoilState(bookReviewState);
-  const reviewData = bookReviews.find(review => String(review.id) === id);
+  const matchedReview = bookReviews.find(review => String(review.id) === id);
   
   const navigate = useNavigate();
+
   const deleteReview = () => {
-    setBookReviews(reviews => reviews.filter(review => review.id !== id));
-    alert('삭제되었습니다.');
-    navigate('/record');
-  }
+    setBookReviews((reviews) => reviews.filter((review) => review.id !== id));
+  };
+
+  const handleDelBtnClick = () => {
+    try {
+      deleteReview();
+      notify({ type: "default", text: "기록이 삭제되었습니다." });
+      navigate('/record');
+    } catch (error) {
+      console.error(error);
+      notify({ type: "error", text: "다시 시도해 주세요." });
+    }
+  };
 
   const moveToUpdate = () => {
     navigate(`/record/update/${id}`);
@@ -55,67 +66,67 @@ function RecordView() {
 
   return (
     <>
-      <div className="
-        flex justify-between items-center
-        fixed top-0 left-0 z-10
-        w-full
-        p-3 px-5 pr-0
-        bg-black
-      ">
-        <BtnBack path={-1} />
-        <BtnEdit openModal={openModal} />
-      </div>
-
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <div>
-          <button
-            className="flex items-center gap-4 w-full px-10 py-5 border-b"
-            onClick={moveToUpdate}
-          >
-            <img className="w-6" src={IconEdit} alt="펜 아이콘" />
-            <span className="font-semibold text-gray-600">수정하기</span>
-          </button>
-          <button
-            className="flex items-center gap-4 w-full px-10 py-5"
-            onClick={deleteReview}
-          >
-            <img className="w-6" src={IconDel} alt="휴지통 아이콘" />
-            <span className="font-semibold text-gray-600">삭제하기</span>
-          </button>
+    {matchedReview && (
+      <>
+        <div className="
+          flex justify-between items-center
+          fixed top-0 left-0 z-10
+          w-full
+          p-3 px-5 pr-0
+          bg-black
+        ">
+          <BtnBack path={-1} />
+          <BtnEdit openModal={openModal} />
         </div>
-      </Modal>
 
-      {reviewData && (
-        <>
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <div>
+            <button
+              className="flex items-center gap-4 w-full px-10 py-5 border-b"
+              onClick={moveToUpdate}
+            >
+              <img className="w-6" src={IconEdit} alt="펜 아이콘" />
+              <span className="font-semibold text-gray-600">수정하기</span>
+            </button>
+            <button
+              className="flex items-center gap-4 w-full px-10 py-5"
+              onClick={handleDelBtnClick}
+            >
+              <img className="w-6" src={IconDel} alt="휴지통 아이콘" />
+              <span className="font-semibold text-gray-600">삭제하기</span>
+            </button>
+          </div>
+        </Modal>
+
         <div className="rounded-br-[48px] py-10 pl-5 bg-black">
           <BookInfoBox
-            thumbnail={reviewData.img}
-            title={reviewData.title}
-            authors={reviewData.authors}
-            publisher={reviewData.publisher}
+            thumbnail={matchedReview.img}
+            title={matchedReview.title}
+            authors={matchedReview.authors}
+            publisher={matchedReview.publisher}
           />
         
           <div className="flex justify-center">
             <ProgressLabel
-              htmlFor={reviewData.cat}
-              selectedOption={reviewData.cat}
+              htmlFor={matchedReview.cat}
+              selectedOption={matchedReview.cat}
               view={true}
             />
           </div>
           <ReadingPeriod
-            startDate={reviewData.startDate}
-            endDate={reviewData.endDate}
+            startDate={matchedReview.startDate}
+            endDate={matchedReview.endDate}
             readonly={true}
           />
         </div>
 
         <div className="layout px-4 py-6 mb-40 bg-gray-100">
-          <p className="whitespace-pre-line text-sm">{reviewData.review}</p>
+          <p className="whitespace-pre-line text-sm">{matchedReview.review}</p>
         </div>
-        </>
-      )}
-    
-      <Nav />
+        
+        <Nav />
+      </>
+    )}
     </>
   )
 };
