1122b04b7724252677e14621bf2f455442ce68dd
misobae <misobae0731@gmail.com>
2024-04-30 17:47:56 +0900

    Feat: NoBook 컴포넌트 margin값 수정

--- src/components/NoBook.tsx
+++ src/components/NoBook.tsx
@@ -11,15 +11,15 @@ function NoBook({ text }: NoBookProps) {
     <div className="flex flex-col justify-center items-center w-full h-[60vh]">
       <div className="
         flex items-center justify-center
-        w-[200px] h-[200px]
+        w-[200px] h-[200px] mb-10
       bg-slate-200
         rounded-full
       ">
         <img className="w-[80px]" src={IconBook} alt="책 아이콘" />
       </div>
-      <p className="text-center text-sm mt-8 whitespace-pre-line">{text}</p>
+      <p className="text-center text-sm mb-8 whitespace-pre-line">{text}</p>
       <Link to="/search">
-        <div className="text-center mt-10">
+        <div className="text-center">
           <Btn text="검색하기" />
         </div>
       </Link>
