c75d52d2aebe0bd9dd11fd36a946d769a0103966
misobae <misobae0731@gmail.com>
2024-05-08 14:15:00 +0900

    Refactor: 기록 검색 페이지 함수 분리 및 함수명 수정

--- src/components/record/RecordSearchBox.tsx
+++ src/components/record/RecordSearchBox.tsx
@@ -13,32 +13,53 @@ function RecordSearchBox() {
   const [searchedWord, setSearchedWord] = useRecoilState(searchedReviewWordState);
   
   const navigate = useNavigate();
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
+  // list 페이지에서 검색할 경우 list search 페이지로 이동
+  const moveToListSearchPage = () => {
     if (location.pathname !== '/list/search') {
       navigate('/list/search');
     }
+  };
 
+  // 검색어 상태 업데이트
+  const updateSearchedWordState = () => {
     setSearchedWord(searchWord);
+  };
+
+  const clearSearchInput = () => {
     setSearchWord("");
   };
-  
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchWord(e.target.value);
   };
 
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    moveToListSearchPage();
+    updateSearchedWordState();
+    clearSearchInput();
+  };
+
+  const matchedReviews = bookReviews.filter((review) => {
+    // 모든 리뷰의 작가 데이터 중 검색어와 일치하는 작가가 있으면 true 반환
+    const authorMatched = review.authors.some((author) =>
+      author.toLowerCase().includes(searchedWord.toLowerCase())
+    );
+  
+    // 모든 리뷰의 타이틀 데이터 중 검색어와 일치하는 값이 있다면 true 반환
+    const titleMatched = review.title.toLowerCase().includes(searchedWord.toLowerCase());
+
+    return authorMatched || titleMatched;
+  });
+
+  // 검색 결과와 일치하는 리뷰 업데이트
+  const updateSearchedReviews = () => {
+    setSearchedReviews(matchedReviews);
+  };
+
   useEffect(() => {
-    const selectedReviews = bookReviews.filter((review) => {
-      const authorMatch = review.authors.some((author) =>
-        author.toLowerCase().includes(searchedWord.toLowerCase())
-      );
-      return (
-        review.title.toLowerCase().includes(searchedWord.toLowerCase()) ||
-        authorMatch
-      );
-    });
-    setSearchedReviews(selectedReviews);
+    updateSearchedReviews();
   }, [searchedWord]);
 
   return (
