d01a4374af57d1e948de5569a828d2c7f71762ff
misobae <misobae0731@gmail.com>
2024-05-11 16:40:05 +0900

    Feat: Lazy Loading 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Route, Routes, useLocation } from "react-router-dom";
 import getMetaData from "./utils/metaData";
 
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import RecordView from "./pages/RecordView";
-import RecordList from "./pages/RecordList";
-import RecordListCat from "./pages/RecordListCat";
-import RecordWrite from "./pages/RecordWrite";
-import RecordUpdate from "./pages/RecordUpdate";
-import Statistics from "./pages/Statistics";
-import Search from "./pages/Search";
-import RecordSearch from "./pages/RecordSearch";
-import RecordRedirect from "./utils/recordRedirect";
-
-import Toast from "./components/common/Toast";
 import Nav from "./components/layout/Nav";
+import Toast from "./components/common/Toast";
+
+const NotFound = lazy(() => import('./pages/NotFound'));
+const RecordView = lazy(() => import('./pages/RecordView'));
+const RecordList = lazy(() => import('./pages/RecordList'));
+const RecordRedirect = lazy(() => import('./utils/recordRedirect'));
+const RecordListCat = lazy(() => import('./pages/RecordListCat'));
+const RecordWrite = lazy(() => import('./pages/RecordWrite'));
+const RecordUpdate = lazy(() => import('./pages/RecordUpdate'));
+const Statistics = lazy(() => import('./pages/Statistics'));
+const Search = lazy(() => import('./pages/Search'));
+const RecordSearch = lazy(() => import('./pages/RecordSearch'));
 
 function App() {
   const location = useLocation();
@@ -40,17 +40,57 @@ function App() {
 
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={ <Home /> } />
-        <Route path="/list" element={<RecordRedirect />} />
-        <Route path="/list" element={<RecordList />} >
-          <Route path=":category" element={<RecordListCat />} />
+        <Route path="/list" element={
+          <Suspense>
+            <RecordRedirect />
+          </Suspense>
+        } />
+        <Route path="/list" element={
+          <Suspense>
+            <RecordList />
+          </Suspense>
+        }>
+          <Route path=":category" element={
+            <Suspense>
+              <RecordListCat />
+            </Suspense>
+          } />
         </Route>
-        <Route path="/list/search" element={ <RecordSearch /> } />
-        <Route path="/record/:id" element={ <RecordView /> } />
-        <Route path="/record/write/:id" element={ <RecordWrite /> } />
-        <Route path="/record/update/:id" element={ <RecordUpdate /> } />
-        <Route path="/statistics" element={ <Statistics /> } />
-        <Route path="/search" element={ <Search /> } />
-        <Route path="/*" element={ <NotFound /> } />
+        <Route path="/list/search" element={
+          <Suspense>
+            <RecordSearch />
+          </Suspense>
+        } />
+        <Route path="/record/:id" element={ 
+          <Suspense>
+            <RecordView />
+          </Suspense>
+        } />
+        <Route path="/record/write/:id" element={ 
+          <Suspense>
+            <RecordWrite />
+          </Suspense>
+        } />
+        <Route path="/record/update/:id" element={
+          <Suspense>
+            <RecordUpdate />
+          </Suspense>
+        } />
+        <Route path="/statistics" element={
+          <Suspense>
+            <Statistics />
+          </Suspense>
+        } />
+        <Route path="/search" element={ 
+          <Suspense>
+            <Search />
+          </Suspense>
+        } />
+        <Route path="/*" element={
+          <Suspense>
+            <NotFound />
+          </Suspense>
+        } />
       </Routes>
 
       {
