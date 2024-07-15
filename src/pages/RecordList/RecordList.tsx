e67c2dca85f3d0cf142d7f0b3881cb0b16ce3d59
misobae <misobae0731@gmail.com>
2024-07-15 17:00:04 +0900

    Refactor: RecordList에서 RecordListCat으로 카테고리 탭과 정렬 버튼 이동

--- src/pages/RecordList/RecordList.tsx
+++ src/pages/RecordList/RecordList.tsx
@@ -1,20 +1,8 @@
 import { Outlet } from "react-router-dom";
 
-import TabBtn from "./components/TabBtn";
-import SortBtn from "./components/SortBtn";
-
 function RecordList() {
   return (
     <div className="layout">
-      <div className="flex justify-between items-center mb-8">
-        <div className="flex items-center text-sm">
-          <TabBtn cat="all" title="전체" />
-          <TabBtn cat="reading" title="읽고 있는 책" />
-          <TabBtn cat="wantToRead" title="읽고 싶은 책" />
-          <TabBtn cat="read" title="읽은 책" />
-        </div>
-        <SortBtn />
-      </div>
       <Outlet />
     </div>
   )
