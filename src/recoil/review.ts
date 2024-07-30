7907c401175f2a6a9ee696bc41e0d0d96ae48a48
misobae <misobae0731@gmail.com>
2024-07-30 09:25:39 +0900

    Feat: 데이터의 날짜 필드를 Date 객체로 변환하는 AtomEffect 추가

--- src/recoil/review.ts
+++ src/recoil/review.ts
@@ -1,21 +1,34 @@
-import { atom } from "recoil";
+import { atom, AtomEffect } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { Review } from "../types/review";
 
 const { persistAtom } = recoilPersist();
 
+const convertDates: AtomEffect<Review[]> = ({ setSelf }) => {
+  setSelf((storedValue) => {
+    if (Array.isArray(storedValue)) {
+      return storedValue.map(review => ({
+        ...review,
+        startDate: review.startDate instanceof Date ? review.startDate : new Date(review.startDate),
+        endDate: review.endDate instanceof Date ? review.endDate : new Date(review.endDate)
+      }));
+    }
+    return storedValue;
+  });
+};
+
 // 리뷰
 export const bookReviewState = atom<Review[]>({
   key: 'bookReviewState',
   default: [],
-  effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom, convertDates],
 });
 
 // 선택된 리뷰
 export const selectedReviewState = atom<Review[]>({
   key: 'selectedReviewState',
   default: [],
-  effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom, convertDates],
 });
 
 // 검색된 리뷰
